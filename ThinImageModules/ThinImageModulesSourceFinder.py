b2d6b980d2a957a3ff24941b25152451ee397bbf
cgerke <chris.gerke@gmail.com>
2014-07-29 16:14:33 +1000

    credits
    credits.

--- ThinImageModules/ThinImageModulesSourceFinder.py
+++ ThinImageModules/ThinImageModulesSourceFinder.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# chris.gerke@gmail.com adapted from AutoPkgSourceFinder.py credits above ^
 
 import os
 import glob
