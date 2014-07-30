90f658dd614d0b711d8389a4454ad929b4b9ae4b
chris.gerke@gmail.com <will-i-am-79>
2014-07-30 20:43:39 +1000

    Credits
    Practically a copy of the processor, just added sccm binary and
    switches.

--- ThinImageModules/cmmacCreator.py
+++ ThinImageModules/cmmacCreator.py
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+'''
+Adapted from the FlatPkgPacker.py AutoPKG processor.
+'''
+
 import os
 import shutil
 import subprocess
